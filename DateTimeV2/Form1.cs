e309275c34e4212ff19a2bcc33c49ab43adc2088
Raito <145450869+Q7nurs3vim@users.noreply.github.com>
2024-01-02 10:21:13 +0300

    Add files via upload

---
+++ DateTimeV2/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DateTimeV2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKarşılaştır_Click(object sender, EventArgs e)
+        {
+            DateTime bugun = DateTime.Now;
+            label1.Text = bugun.ToString();
+            label2.Text = bugun.AddDays(5).ToString() + " 5 gün eklendi."; //Bugünün tarihine 5 gün ekle.
+            label3.Text = bugun.AddMonths(2).ToString() + " 2 ay eklendi.";//Bugünün tarihine 2 ay ekle.
+            label4.Text = bugun.AddYears(2).ToString() + " 2 yıl eklendi.";//Bugünün tarihine 2 yıl ekle.
+            label5.Text = bugun.AddHours(3).ToString() + " 3 saat eklendi.";//Anlık saate 3 saat ekle
+            label6.Text = bugun.AddMinutes(15).ToString() + " 15 dakika eklendi.";//Anlık saate 15 dakika ekle.
+            label7.Text = bugun.AddSeconds(20).ToString() + " 20 saniye eklendi.";//Anlık saate 20 saniye ekle.
+        }
+    }
+
+    
+}
+
